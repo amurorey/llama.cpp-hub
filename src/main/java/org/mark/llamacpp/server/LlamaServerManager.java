f884361549a5f06c3973d30d3eea800a0a8a09b8
IIIIIllllIIIIIlllll <2432896620@qq.com>
2026-06-08 09:27:57 +0800

    修改：设置llama.cpp进程的日志等级为-lv 4，更详细一点

--- src/main/java/org/mark/llamacpp/server/LlamaServerManager.java
+++ src/main/java/org/mark/llamacpp/server/LlamaServerManager.java
@@ -1415,6 +1415,8 @@ public class LlamaServerManager {
 		sb.append(" --timeout 36000");
 		// 允许任意IP地址访问
 		sb.append(" --host 0.0.0.0");
+		// 输出详细日志
+		sb.append("-lv 4");
 		
 		return sb.toString().trim();
 	}
